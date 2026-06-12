f15945715deab361b231dbb4e7950d6461fc282e
Nikhil <nikhilsharma5122006@gmail.com>
2026-06-12 15:30:18 +0530

    Fix python search path for Streamlit Cloud

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -15,6 +15,9 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fifa_predictor.models.predict import (
     predict_match,
     _team_astro_score,
